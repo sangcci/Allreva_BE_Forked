c0004344291c1c38cad696952cc9255d013b9c93
kwanse <129512238+kwanse@users.noreply.github.com>
2025-03-06 16:55:20 +0900

    [fix] SSE HttpResponse 관련 코드 수정 (#219)

--- src/main/java/com/backend/allreva/chatting/notification/ChatNotificationController.java
+++ src/main/java/com/backend/allreva/chatting/notification/ChatNotificationController.java
@@ -2,9 +2,7 @@ package com.backend.allreva.chatting.notification;
 
 import com.backend.allreva.auth.security.AuthMember;
 import com.backend.allreva.member.command.domain.Member;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,21 +14,10 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 @RestController
 public class ChatNotificationController {
 
-    @Value("${url.front.protocol}")
-    private String frontProtocol;
-    @Value("${url.front.domain}")
-    private String frontDomain;
-
     private final MessageSseService messageSseService;
 
     @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public SseEmitter connectNotification(
-            HttpServletResponse response,
-            @AuthMember final Member member
-    ) {
-        response.setHeader("Access-Control-Allow-Origin", "https://localhost:3000");
-        response.setHeader("Access-Control-Allow-Origin", frontProtocol + "://" + frontDomain);
-        response.setHeader("Access-Control-Allow-Credentials", "true");
+    public SseEmitter connectNotification(@AuthMember final Member member) {
         return messageSseService.connect(member.getId());
     }
 
