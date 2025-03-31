b5e38754c743ac32a36a89705912ed71d3af8453
kwanse <khan67781397@gmail.com>
2025-03-31 19:24:35 +0900

    [hotfix] 메시지 Content 에 @Setter 애노테이션 추가

--- src/main/java/com/backend/allreva/chatting/message/domain/value/Content.java
+++ src/main/java/com/backend/allreva/chatting/message/domain/value/Content.java
@@ -3,7 +3,9 @@ package com.backend.allreva.chatting.message.domain.value;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Content {
