07d3baae46016d8db408da5588e06547509db5ca
sangcci <pshsh910@gmail.com>
2026-02-22 23:48:24 +0900

    test: Testcontainers PostgreSQL로 통합 테스트 datasource 교체

--- src/test/java/com/backend/allreva/support/IntegrationTestSupport.java
+++ src/test/java/com/backend/allreva/support/IntegrationTestSupport.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import com.backend.allreva.common.config.JpaAuditingConfig;
 
@@ -14,7 +18,12 @@ import com.backend.allreva.common.config.JpaAuditingConfig;
 @ActiveProfiles("test")
 @MockBean(JpaAuditingConfig.class)
 @AutoConfigureMockMvc(addFilters = false)
+@Testcontainers
 public abstract class IntegrationTestSupport {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16");
 
     @Autowired
     protected AsyncAspect asyncAspect;
