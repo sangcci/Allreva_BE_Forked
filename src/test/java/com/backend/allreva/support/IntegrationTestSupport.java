c3b0199086944f16f3a96f75796ec0a3097f43ae
sangcci <pshsh910@gmail.com>
2026-03-01 18:07:04 +0900

    test: 싱글톤 컨테이너 패턴 개선 및 sql init 설정 application.yml으로 위임
    - https://testcontainers.com/guides/testcontainers-container-lifecycle/

--- src/test/java/com/backend/allreva/support/IntegrationTestSupport.java
+++ src/test/java/com/backend/allreva/support/IntegrationTestSupport.java
@@ -1,13 +1,13 @@
 package com.backend.allreva.support;
 
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-
 import com.backend.allreva.common.config.FcmInitializer;
 import com.backend.allreva.common.config.JpaAuditingConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
@@ -21,14 +21,15 @@ import org.testcontainers.containers.PostgreSQLContainer;
 @Import({AsyncAspect.class, FixedClockConfig.class})
 public abstract class IntegrationTestSupport {
 
-    static final PostgreSQLContainer<?> postgres;
-
+    @ServiceConnection
     @SuppressWarnings("resource")
-    static final GenericContainer<?> redis;
+    protected static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16");
+
+    @ServiceConnection(name = "redis")
+    @SuppressWarnings("resource")
+    protected static final GenericContainer<?> redis = new GenericContainer<>("redis:7-alpine").withExposedPorts(6379);
 
     static {
-        postgres = new PostgreSQLContainer<>("postgres:16").withInitScript("db/trgm-indexes.sql");
-        redis = new GenericContainer<>("redis:7-alpine").withExposedPorts(6379);
         postgres.start();
         redis.start();
     }
@@ -36,10 +37,6 @@ public abstract class IntegrationTestSupport {
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url", () -> postgres.getJdbcUrl() + "?stringtype=unspecified");
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
-        registry.add("spring.data.redis.host", redis::getHost);
-        registry.add("spring.data.redis.port", () -> redis.getMappedPort(6379));
     }
 
     @Autowired
