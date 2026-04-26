52d42ddbe963c0062122f7bc852de699294dd458
sangcci <pshsh910@gmail.com>
2026-04-27 00:20:34 +0900

    refactor(ConcertHallErrorCode): CONCERT_HALL_NOT_FOUND 추가

--- src/main/java/com/backend/allreva/module/concert/concert/application/ConcertService.java
+++ src/main/java/com/backend/allreva/module/concert/concert/application/ConcertService.java
@@ -1,9 +1,13 @@
 package com.backend.allreva.module.concert.concert.application;
 
+import com.backend.allreva.common.exception.CustomException;
 import com.backend.allreva.module.concert.concert.application.dto.ConcertDetailResponse;
+import com.backend.allreva.module.concert.concert.domain.Concert;
 import com.backend.allreva.module.concert.concert.domain.ConcertRepository;
+import com.backend.allreva.module.concert.concert.exception.ConcertErrorCode;
 import com.backend.allreva.module.concert.place.domain.ConcertHall;
 import com.backend.allreva.module.concert.place.domain.ConcertHallRepository;
+import com.backend.allreva.module.concert.place.exception.ConcertHallErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,14 +21,12 @@ public class ConcertService {
 
     @Transactional(readOnly = true)
     public ConcertDetailResponse findDetailById(final String concertCode) {
-        return concertRepository
+        Concert concert = concertRepository
                 .findById(concertCode)
-                .map(concert -> {
-                    ConcertHall hall = concertHallRepository
-                            .findByHallCode(concert.getHallCode())
-                            .orElse(null);
-                    return ConcertDetailResponse.from(concert, hall);
-                })
-                .orElse(ConcertDetailResponse.EMPTY);
+                .orElseThrow(() -> new CustomException(ConcertErrorCode.CONCERT_NOT_FOUND));
+        ConcertHall hall = concertHallRepository
+                .findByHallCode(concert.getHallCode())
+                .orElseThrow(() -> new CustomException(ConcertHallErrorCode.CONCERT_HALL_NOT_FOUND));
+        return ConcertDetailResponse.from(concert, hall);
     }
 }
