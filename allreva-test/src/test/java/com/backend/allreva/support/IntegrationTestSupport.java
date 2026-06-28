b86f153c1000758da2fde3879b12151fe35d0c78
sangcci <pshsh910@gmail.com>
2026-06-29 02:05:40 +0900

    test(test): 통합 테스트에서 LocalStack S3를 사용하게 변경

--- allreva-test/src/test/java/com/backend/allreva/support/IntegrationTestSupport.java
+++ allreva-test/src/test/java/com/backend/allreva/support/IntegrationTestSupport.java
@@ -5,12 +5,12 @@ import com.backend.allreva.auth.kakao.KakaoAuthClient;
 import com.backend.allreva.auth.kakao.KakaoUserInfoClient;
 import com.backend.allreva.common.config.JpaAuditingConfig;
 import com.backend.allreva.common.storage.StorageWriter;
-import com.backend.allreva.notification.fcm.FcmInitializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
@@ -20,6 +20,8 @@ import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest(classes = ApiServerApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
@@ -27,6 +29,8 @@ import org.testcontainers.containers.PostgreSQLContainer;
 @AutoConfigureWireMock(port = 0)
 @Import({FixedClockConfig.class})
 public abstract class IntegrationTestSupport {
+
+    private static final String S3_BUCKET = "test-bucket";
 
     @ServiceConnection
     @SuppressWarnings("resource")
@@ -36,26 +40,45 @@ public abstract class IntegrationTestSupport {
     @SuppressWarnings("resource")
     protected static final GenericContainer<?> redis = new GenericContainer<>("redis:7-alpine").withExposedPorts(6379);
 
+    @SuppressWarnings("resource")
+    protected static final LocalStackContainer localStack = new LocalStackContainer(
+                    DockerImageName.parse("localstack/localstack:3.8.1"))
+            .withServices(LocalStackContainer.Service.S3);
+
     static {
         postgres.start();
         redis.start();
+        localStack.start();
+        createS3Bucket();
     }
 
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url", () -> postgres.getJdbcUrl() + "?stringtype=unspecified");
+        registry.add("spring.cloud.aws.s3.bucket", () -> S3_BUCKET);
+        registry.add("spring.cloud.aws.s3.endpoint", () -> localStack
+                .getEndpointOverride(LocalStackContainer.Service.S3)
+                .toString());
+        registry.add("spring.cloud.aws.credentials.access-key", localStack::getAccessKey);
+        registry.add("spring.cloud.aws.credentials.secret-key", localStack::getSecretKey);
+        registry.add("spring.cloud.aws.region.static", localStack::getRegion);
+    }
+
+    private static void createS3Bucket() {
+        try {
+            localStack.execInContainer("awslocal", "s3", "mb", "s3://" + S3_BUCKET);
+        } catch (Exception e) {
+            throw new IllegalStateException("Failed to create LocalStack S3 bucket", e);
+        }
     }
 
     @Autowired
     protected JdbcTemplate jdbcTemplate;
 
     @MockBean
-    protected FcmInitializer fcmInitializer;
-
-    @MockBean
     protected JpaAuditingConfig jpaAuditingConfig;
 
-    @MockBean
+    @SpyBean
     protected StorageWriter storageWriter;
 
     @MockBean
